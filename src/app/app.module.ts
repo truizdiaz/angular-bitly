e433a72d8f6ee6618819eb93d10e8bb0855e60d8
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-21 09:21:39 -0300

    Creacion de componentes y servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShortUrlComponent } from './components/short-url/short-url.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ShortUrlComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
