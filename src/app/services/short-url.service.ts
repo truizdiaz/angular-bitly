1ea94447735fbb618123daa66b5a40ca490269ae
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-21 10:07:31 -0300

    API Bitly

--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -5,5 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ShortUrlService {
 
+  url = 'https://api-ssl.bitly.com/v4/shorten';
+  token = '81c5bc644ff2c3c008f4ea79456cc9d34e7cc806'
   constructor() { }
 }
